be7cb74f898c955f85c68f877ed0883a247b05b2
binod164 <binod7_grg@hotmail.com>
2022-05-02 10:13:04 -0500

    COmplete 7-9

--- practice.sql
+++ practice.sql
@@ -1,25 +1,39 @@
 -- 1.Show first name, last name, and gender of patients who's gender is 'M'
 SELECT first_name,last_name, gender FROM patients
-Where gender = "M";
+WHERE gender = "M";
 
 
 -- 2.Show first name and last name of patients who does not have allergies (null).
 SELECT first_name,last_name FROM patients
-Where allergies IS null;
+WHERE allergies IS null;
 
 --3.Show first name of patients that start with the letter 'C'
 SELECT first_name FROM patients
-Where first_name LIKe "C%";
+WHERE first_name LIKe "C%";
 
 --4.Show first name and last name of patients that weight within the range of 100 to 120 (inclusive)
 SELECT first_name,last_name FROM patients
-Where weight Between 100 and 120;
+WHERE weight Between 100 and 120;
 
 --5.Update the patients table for the allergies column. If the patient's allergies is null then replace it with 'NKA'
 UPDATE patients
 SET allergies = "NKA"
-Where allergies IS null;
+WHERE allergies IS null;
 
 --6.Show first name and last name concatinated into one column to show their full name.
 SELECT CONCAT(first_name,' ', last_name) AS full_name
 FROM patients;
+
+--7.Show first name, last name, and the full province name of each patient.
+SELECT first_name,last_name,province_name FROM patients
+JOIN provinces
+WHERE patients.province_id = provinces.province_id;
+
+--8.Show how many patients have a birth_date with 2010 as the birth year.
+SELECT Count(*) FROM patients
+WHERE YEAR(birth_date) = 2010;
+
+--9.Show the first_name, last_name, and height of the patient with the greatest height.
+SELECT first_name,last_name,height FROM patients
+WHERE height = (SELECT MAX(height) from patients);
+
