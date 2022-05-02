92cb087e296281c4b9d14d81aa613a8348949537
binod164 <binod7_grg@hotmail.com>
2022-05-02 11:50:42 -0500

    COmplete 10-15

--- practice.sql
+++ practice.sql
@@ -37,3 +37,34 @@ WHERE YEAR(birth_date) = 2010;
 SELECT first_name,last_name,height FROM patients
 WHERE height = (SELECT MAX(height) from patients);
 
+--10.Show all columns for patients who have one of the following patient_ids:
+-- 1,45,534,879,1000
+
+SELECT * FROM patients
+WHERE patient_id in (1,45,534,879,1000);
+
+--11.Show the total number of admissions
+SELECT COunt(*) from admissions;
+
+--12.Show unique birth years from patients and order them by ascending.
+SELECT DIstinct YEAR(birth_date) from patients
+ORDER BY YEAR(birth_date);
+
+--13.Show unique first names from the patients table which only occurs once in the list.
+-- For example, if two or more people are named 'John' in the first_name column then don't include their name in the output list. If only 1 person is named 'Leo' then include them in the output.
+SELECT first_name
+  FROM patients
+  GROUP BY first_name
+  HAVING count(*) = 1;
+
+  --14.Show patient_id and first_name from patients where their first_name start and ends with 's' and is atleast 5 characters long.
+  SELECT patient_id,first_name
+  FROM patients
+  Where first_name like "s___%s";
+
+  --15.Show patient_id, first_name, last_name from patients whos primary_diagnosis is 'Dementia'.Primary diagnosis is stored in the admissions table.
+SELECT patients.patient_id,first_name, last_name
+  FROM patients
+  JOIN admissions
+  ON patients.patient_id = admissions.patient_id
+  Where primary_diagnosis = "Dementia";
