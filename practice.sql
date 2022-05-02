3f0a649392b2af12da5cb39f0a7a0f9d1fb65396
binod164 <binod7_grg@hotmail.com>
2022-05-01 20:02:34 -0500

    Complete 3 to 6

--- practice.sql
+++ practice.sql
@@ -10,3 +10,16 @@ Where allergies IS null;
 --3.Show first name of patients that start with the letter 'C'
 SELECT first_name FROM patients
 Where first_name LIKe "C%";
+
+--4.Show first name and last name of patients that weight within the range of 100 to 120 (inclusive)
+SELECT first_name,last_name FROM patients
+Where weight Between 100 and 120;
+
+--5.Update the patients table for the allergies column. If the patient's allergies is null then replace it with 'NKA'
+UPDATE patients
+SET allergies = "NKA"
+Where allergies IS null;
+
+--6.Show first name and last name concatinated into one column to show their full name.
+SELECT CONCAT(first_name,' ', last_name) AS full_name
+FROM patients;
